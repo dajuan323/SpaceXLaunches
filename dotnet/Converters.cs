c2c0b7533a7d189385c8dafb66778e683e66e6fa
dajuan323 <macaro.martin@gmail.com>
2023-05-01 02:26:19 -0400

    Add .NET portion of the app

---
+++ dotnet/Converters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HDMSpaceX.Converters
+{
+    public class EmptyStringToNullJsonConverter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String && string.IsNullOrWhiteSpace(reader.GetString()))
+            {
+                return 0;
+            }
+
+            return reader.GetInt32();
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value);
+        }
+    }
+
+    public class ConverterForPayloadMassLbs : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return 0; // or any other default value you want to use for nulls
+            }
+
+            return reader.GetInt32();
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value);
+        }
+    }
+
+    public class ConverterForPeriapsisKm : JsonConverter<int?>
+    {
+        public override int? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return null;
+            }
+
+            return reader.GetInt32();
+        }
+
+        public override void Write(Utf8JsonWriter writer, int? value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet/Models/OrbitParams.cs
@@ -0,0 +1,57 @@
+﻿using HDMSpaceX.Converters;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+
+namespace HDMSpaceX.Models
+{
+    public class OrbitParams
+    {
+        [JsonProperty("reference_system")]
+        public string? ReferenceSystem { get; set; }
+
+        [JsonProperty("regime")]
+        public string? Regime { get; set; }
+
+        [JsonProperty("longitude")]
+        public object? Longitude { get; set; }
+
+        [JsonProperty("semi_major_axis_km")]
+        public object? SemiMajorAxisKm { get; set; }
+
+        [JsonProperty("eccentricity")]
+        public object? Eccentricity { get; set; }
+
+        [JsonPropertyName("periapsis_km")]
+        [System.Text.Json.Serialization.JsonConverter(typeof(ConverterForPeriapsisKm))]
+        public int? PeriapsisKm { get; set; }
+
+        [JsonProperty("apoapsis_km")]
+        public float? ApoapsisKm { get; set; }
+
+        [JsonProperty("inclination_deg")]
+        public float? InclinationDeg { get; set; }
+
+        [JsonProperty("period_min")]
+        public object? PeriodMin { get; set; }
+
+        [JsonProperty("lifespan_years")]
+        public object? LifespanYears { get; set; }
+
+        [JsonProperty("epoch")]
+        public object? Epoch { get; set; }
+
+        [JsonProperty("mean_motion")]
+        public object? MeanMotion { get; set; }
+
+        [JsonProperty("raan")]
+        public object? Raan { get; set; }
+
+        [JsonProperty("arg_of_pericenter")]
+        public object? ArgOfPericenter { get; set; }
+
+        [JsonProperty("mean_anomaly")]
+        public object? MeanAnomaly { get; set; }
+    }
+
+}
